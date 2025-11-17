f15292de0a7f4755016fa0afcc4b2698f7e40a88
Fabricio Jacome <112516550+fabriciomj@users.noreply.github.com>
2025-11-17 14:18:06 -0300

    Cria as views genéricas dos Models e Serializers

--- fabricio/mini_ifood/views.py
+++ fabricio/mini_ifood/views.py
@@ -1,7 +1,28 @@
 from django.contrib.auth.models import Group, User
-from rest_framework import permissions, viewsets
+from rest_framework import generics, permissions, viewsets
 
-from fabricio.mini_ifood.serializers import GroupSerializer, UserSerializer
+from fabricio.mini_ifood.models import (
+    Carrinho,
+    Cliente,
+    Endereco,
+    Estabelecimento,
+    FormaPagamento,
+    Oferta,
+    Pedido,
+    Prato,
+)
+from fabricio.mini_ifood.serializers import (
+    CarrinhoSerializer,
+    ClienteSerializer,
+    EnderecoSerializer,
+    EstabelecimentoSerializer,
+    FormaPagamentoSerializer,
+    GroupSerializer,
+    OfertaSerializer,
+    PedidoSerializer,
+    PratoSerializer,
+    UserSerializer,
+)
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -18,3 +39,83 @@ class GroupViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all().order_by("name")
     serializer_class = GroupSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+
+class CarrinhoList(generics.ListCreateAPIView):
+    queryset = Carrinho.objects.all()
+    serializer_class = CarrinhoSerializer
+
+
+class CarrinhoDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Carrinho.objects.all()
+    serializer_class = CarrinhoSerializer
+
+
+class ClienteList(generics.ListCreateAPIView):
+    queryset = Cliente.objects.all()
+    serializer_class = ClienteSerializer
+
+
+class ClienteDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Cliente.objects.all()
+    serializer_class = ClienteSerializer
+
+
+class EnderecoList(generics.ListCreateAPIView):
+    queryset = Endereco.objects.all()
+    serializer_class = EnderecoSerializer
+
+
+class EnderecoDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Endereco.objects.all()
+    serializer_class = EnderecoSerializer
+
+
+class EstabelecimentoList(generics.ListCreateAPIView):
+    queryset = Estabelecimento.objects.all()
+    serializer_class = EstabelecimentoSerializer
+
+
+class EstabelecimentoDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Estabelecimento.objects.all()
+    serializer_class = EstabelecimentoSerializer
+
+
+class FormaPagamentoList(generics.ListCreateAPIView):
+    queryset = FormaPagamento.objects.all()
+    serializer_class = FormaPagamentoSerializer
+
+
+class FormaPagamentoDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = FormaPagamento.objects.all()
+    serializer_class = FormaPagamentoSerializer
+
+
+class OfertaList(generics.ListCreateAPIView):
+    queryset = Oferta.objects.all()
+    serializer_class = OfertaSerializer
+
+
+class OfertaDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Oferta.objects.all()
+    serializer_class = OfertaSerializer
+
+
+class PedidoList(generics.ListCreateAPIView):
+    queryset = Pedido.objects.all()
+    serializer_class = PedidoSerializer
+
+
+class PedidoDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Pedido.objects.all()
+    serializer_class = PedidoSerializer
+
+
+class PratoList(generics.ListCreateAPIView):
+    queryset = Prato.objects.all()
+    serializer_class = PratoSerializer
+
+
+class PratoDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Prato.objects.all()
+    serializer_class = PratoSerializer
