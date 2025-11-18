e8f7d20ac26e4ee3a38715119a72514ff170c8e9
Fabricio Jacome <112516550+fabriciomj@users.noreply.github.com>
2025-11-18 19:31:18 -0300

    Atribui permissões aos ViewSets

--- mini_ifood/views.py
+++ mini_ifood/views.py
@@ -9,6 +9,7 @@ from mini_ifood.models import (
     Pedido,
     Prato,
 )
+from mini_ifood.permissions import IsOwner, IsOwnerOrAuthenticated
 from mini_ifood.serializers import (
     CarrinhoSerializer,
     ClienteSerializer,
@@ -23,33 +24,40 @@ from mini_ifood.serializers import (
 class CarrinhoViewSet(viewsets.ModelViewSet):
     queryset = Carrinho.objects.all()
     serializer_class = CarrinhoSerializer
+    permission_classes = [IsOwner]
 
 
 class ClienteViewSet(viewsets.ModelViewSet):
     queryset = Cliente.objects.all()
     serializer_class = ClienteSerializer
+    permission_classes = [IsOwner]
 
 
 class EstabelecimentoViewSet(viewsets.ModelViewSet):
     queryset = Estabelecimento.objects.all()
     serializer_class = EstabelecimentoSerializer
+    permission_classes = [IsOwnerOrAuthenticated]
 
 
 class FormaPagamentoViewSet(viewsets.ModelViewSet):
     queryset = FormaPagamento.objects.all()
     serializer_class = FormaPagamentoSerializer
+    permission_classes = [IsOwner]
 
 
 class OfertaViewSet(viewsets.ModelViewSet):
     queryset = Oferta.objects.all()
     serializer_class = OfertaSerializer
+    permission_classes = [IsOwnerOrAuthenticated]
 
 
 class PedidoViewSet(viewsets.ModelViewSet):
     queryset = Pedido.objects.all()
     serializer_class = PedidoSerializer
+    permission_classes = [IsOwner]
 
 
 class PratoViewSet(viewsets.ModelViewSet):
     queryset = Prato.objects.all()
     serializer_class = PratoSerializer
+    permission_classes = [IsOwnerOrAuthenticated]
