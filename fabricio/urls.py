3fc6f29b8d0a9895314dd82090ecc501300443df
Fabricio Jacome <112516550+fabriciomj@users.noreply.github.com>
2025-11-18 19:27:34 -0300

    Remove url de api-auth
    Visto que a autenticação será feita com JWT.

--- fabricio/urls.py
+++ fabricio/urls.py
@@ -2,5 +2,4 @@ from django.urls import include, path
 
 urlpatterns = [
     path("", include("mini_ifood.urls")),
-    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
 ]
