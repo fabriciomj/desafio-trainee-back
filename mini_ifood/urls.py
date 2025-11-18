ab2011bd553cce5fc129890cee3da088b66259b5
Fabricio Jacome <112516550+fabriciomj@users.noreply.github.com>
2025-11-18 17:10:56 -0300

    Registra os ViewSets no Router

--- mini_ifood/urls.py
+++ mini_ifood/urls.py
@@ -4,6 +4,20 @@ from rest_framework import routers
 from mini_ifood import views
 
 router = routers.DefaultRouter()
-router.register(r"users", views.UserViewSet)
+
+to_register = [
+    (r"users", views.UserViewSet),
+    (r"carrinhos", views.CarrinhoViewSet),
+    (r"clientes", views.ClienteViewSet),
+    (r"enderecos", views.EnderecoViewSet),
+    (r"estabelecimentos", views.EstabelecimentoViewSet),
+    (r"pagamentos", views.FormaPagamentoViewSet),
+    (r"ofertas", views.OfertaViewSet),
+    (r"pedidos", views.PedidoViewSet),
+    (r"pratos", views.PratoViewSet),
+]
+
+for tup in to_register:
+    router.register(tup[0], tup[1])
 
 urlpatterns = [path("", include(router.urls))]
