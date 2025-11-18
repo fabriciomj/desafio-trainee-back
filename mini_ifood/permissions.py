138f928972f2f67ec627344b6eabfd826c848fc4
Fabricio Jacome <112516550+fabriciomj@users.noreply.github.com>
2025-11-18 19:32:43 -0300

    Adiciona uma exceção para o Ruff
    A assinatura da função sobrecarregada contém um parâmetro que não foi utilizado.

--- mini_ifood/permissions.py
+++ mini_ifood/permissions.py
@@ -4,14 +4,14 @@ from rest_framework import permissions
 class IsOwner(permissions.BasePermission):
     """Custom permission to only allow owners of an object to edit it."""
 
-    def has_object_permission(self, request, view, obj):
+    def has_object_permission(self, request, view, obj):  # noqa: ARG002
         return obj.owner == request.user
 
 
 class IsOwnerOrAuthenticated(permissions.BasePermission):
     """Permissão que permite ao owner de um objeto editá-lo e aos usuários autenticados visualizá-lo."""
 
-    def has_object_permission(self, request, view, obj):
+    def has_object_permission(self, request, view, obj):  # noqa: ARG002
         if (
             request.method in permissions.SAFE_METHODS
             and request.user
