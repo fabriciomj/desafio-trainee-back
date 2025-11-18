ddd0abd9669e905f5c84f47d8877e550efbebdbe
Fabricio Jacome <112516550+fabriciomj@users.noreply.github.com>
2025-11-18 19:31:00 -0300

    Cria uma nova permissão IsOwnerOrAuthenticated

--- mini_ifood/permissions.py
+++ mini_ifood/permissions.py
@@ -6,3 +6,17 @@ class IsOwner(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
         return obj.owner == request.user
+
+
+class IsOwnerOrAuthenticated(permissions.BasePermission):
+    """Permissão que permite ao owner de um objeto editá-lo e aos usuários autenticados visualizá-lo."""
+
+    def has_object_permission(self, request, view, obj):
+        if (
+            request.method in permissions.SAFE_METHODS
+            and request.user
+            and request.user.is_authenticated
+        ):
+            return True
+
+        return obj.owner == request.user
